0c04478f2d296f6b290d342be8ac343427382afd
Stefan Hamminga <stefan@prjct.net>
2015-12-24 12:19:58 +1100

    Initial version

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # tz-bounce
 Node.js based timezone bounce server. Uses the public IP to reply a matched location and timezone.
+
+## Installation
+
+```bash
+cd mysites
+npm install tz-bounce
+```
+
+or
+
+```bash
+cd mysites
+git clone https://github.com/StefanHamminga/tz-bounce.git
+```
+
+## Usage
+
+```bash
+curl https://url
+```
+
+will reply a [YAML](https://en.wikipedia.org/wiki/YAML) formatted message containing matched information.
+
+## License & repository
+
+This project is available on [GitHub](https://github.com/StefanHamminga/tz-bounce) and [npm](https://www.npmjs.com/package/tz-bounce)
+
+Copyright 2015 [Stefan Hamminga](stefan@prjct.net) - [prjct.net](https://prjct.net)
+
+License: [GPL v3.0](https://www.gnu.org/licenses/gpl-3.0.en.html). The license text is included in the repository.
---
+++ index.js
@@ -0,0 +1,55 @@
+#!/usr/bin/env node
+
+/* TZ-Bounce
+This program is free software: you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software
+Foundation, either version 3 of the License, or (at your option) any later
+version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with
+this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+"use strict";
+const api_name  = 'tz-bounce yaml v1.0.0';
+const port      = process.env.TZBOUNCEPORT || process.argv[2] || 8000;
+const requestIp = require('request-ip');
+const geoip     = require('geoip-lite');
+const tzlookup  = require("tz-lookup");
+const http      = require('http');
+
+// const util      = require('util');
+// const inspect   = function(desc, item) { if (!(item)) {item = desc; desc = "";} console.log(desc, util.inspect(item, {colors: true}))};
+
+// Configure our HTTP server to respond with Hello World to all requests.
+const server    = http.createServer(function (request, response) {
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    let ip    = requestIp.getClientIp(request);
+    // inspect("IP address: ", ip);
+    let loc   = geoip.lookup(ip);
+    // inspect("Geo location: ", loc);
+    let tz;
+    if (loc && loc.ll) {
+        tz = tzlookup(loc.ll[0], loc.ll[1]);
+    }
+    // inspect("Timezone ", tz);
+    response.end("Api: " + api_name + "\n" +
+                 "PublicIP: " + (ip || "null") + "\n" +
+                 "Timezone: " + (tz || "null") + "\n" +
+                 "Location: " + ((loc) ?
+                     ("\n\tCountry: " + (loc.country || "null") + "\n" +
+                 "\tRegion: " + (loc.region || "null") + "\n" +
+                 "\tCity: " + (loc.city || "null") + "\n" +
+                 "\tMetro: " + (loc.metro || "null") + "\n" +
+                 "\tLatitude: " + (loc.ll[0] || "null") + "\n" +
+                 "\tLongitude: " + (loc.ll[1] || "null") + "\n") : "null\n"));
+});
+
+server.listen(port);
+if (!(process.env.TZBOUNCEPORT) && !(process.argv[2]))
+    console.log("Use `TZBOUNCEPORT=nnnn ./index.js` or `./index.js nnnn` to set the listening port")
+console.log("TZ-bounce server listening on port: " + port);
